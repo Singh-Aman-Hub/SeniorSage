5f9387045915a0abb88d2b8a7af4e61933399316
Singh-Aman-Hub <amansingh102006@gmail.com>
2026-01-22 11:53:54 +0530

    template

---
+++ seniorsage/src/main/java/com/example/seniorsage/SeniorsageApplication.java
@@ -0,0 +1,13 @@
+package com.example.seniorsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorsageApplication.class, args);
+	}
+
+}
---
+++ seniorsage/src/test/java/com/example/seniorsage/SeniorsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seniorsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
